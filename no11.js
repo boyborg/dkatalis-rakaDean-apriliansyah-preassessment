2e202fc34032c159d1f0c11e1c213d0d4f7ad2ef
Yolandi Bintang Putra <yolandiputra@gmail.com>
2019-11-13 17:22:21 +0700

    second commit

---
+++ no11.js
@@ -0,0 +1,13 @@
+//Problem statement
+//Given an Array of strings, use Array#reduce to create an object 
+//that contains the number of times each string occured in the array.
+
+var inputWords = ['Apple', 'Banana', 'Apple', 'Durian', 'Durian', 'Durian']
+
+function countWords(arr) {
+  return arr.reduce(function(countMap, word) {
+    countMap[word] = ++countMap[word] || 1 // your implementation here
+    return countMap
+  }, {}) // second argument to reduce initialises countMap to {}
+}
+console.log(countWords(inputWords))
